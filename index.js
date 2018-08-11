a90eb869124b9f85ca7ac08ba7f3f60c3e2cde0e
Ann Siebecker <asiebecker@hotmail.com>
2018-08-11 17:08:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-function previewFirstName(event){
+function previewFirstName(event) {
   console.log(event.srcElement.value)
-  document.getElementByID('first_name_preview').innerText = event.srcElement.value
+  document.getElementById('first_name_preview').innerText = event.srcElement.value
 }
+document.getElementById('first_name').addEventListener('keyup', previewFirstName)
 
-document.getElementById("first_name").addEventListener("keyup", previewFirstName);
+function previewSecondName(event) {
+  console.log(event.srcElement.value)
+  document.getElementById('second_name_preview').innerText = event.srcElement.value
+}
+document.getElementById('second_name').addEventListener('keyup', previewSecondName)
 
+
+
+function previewLastName(event) {
+  console.log(event.srcElement.value)
+  document.getElementById('last_name_preview').innerText = event.srcElement.value
+}
+document.getElementById('last_name').addEventListener('keyup', previewLastName)
